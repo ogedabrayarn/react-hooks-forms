d21775c5f85aad47234abed42655adc816862042
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-10-02 10:05:49 +0300

    completed controlled form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,28 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value)
+
+    // firstName("");
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value)
+
+    // lastName("");
+  }
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
-      <button type="submit">Submit</button>
+      <input type='text' value={firstName} onChange={handleFirstNameChange} />
+      <input type='text' value={lastName} onChange={handleLastNameChange} />
+      <button type='submit'>Submit</button>
     </form>
-  );
+  )
 }
 
 export default Form;
